4242da88881d9f2ec805ecef5de1fa3574e6b239
agagya6y <142376286+agagya6y@users.noreply.github.com>
2023-08-17 18:46:23 +0530

    landing-content.tsx

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -7,7 +7,7 @@ const testimonials = [
     name: "Joel",
     avatar: "J",
     title: "Software Engineer",
-    description: "This is the best application I've ever used!",
+    description: "This is the best application I&apos;ve ever used!",
   },
   {
     name: "Antonio",
